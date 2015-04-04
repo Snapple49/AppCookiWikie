b1694f40b127e6685e77ce2692ff718151dff69f
Snapple49 <oliver-dot-stein@hotmail.com>
2015-04-04 17:58:20 +0200

    Added the project

---
+++ App1/Model/Cooki.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace App1.Common
+{
+    public class Cooki : INotifyPropertyChanged
+    {
+        //The name of the cookie
+        public String Name { get; set; }
+
+        //An image depicting the cookie
+        public String ImgLoc { get; set; }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+        public Cooki GetCopy()
+        {
+            Cooki copy = (Cooki)this.MemberwiseClone();
+            return copy;
+        }
+    }
+}
---
+++ App1/ViewModel/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+//using System.IO.IsolatedStorage;
+
+
+
+namespace App1.Common
+{
+    class ViewModel
+    {
+        public ObservableCollection<Cooki> CookieList { get; set; }
+
+        public void GetCookies()
+        {
+            GetDefaultCookies();
+        }
+
+        public void GetDefaultCookies(){
+            ObservableCollection<Cooki> a = new ObservableCollection<Cooki>();
+
+            a.Add(new Cooki() { Name = "Chocolate chip cookie", ImgLoc = "Assets/ChocolateChip.png" });
+            a.Add(new Cooki() { Name = "Chocolate chip cookie 2", ImgLoc = "Assets/ChocolateChip.png" });
+            a.Add(new Cooki() { Name = "Chocolate chip cookie 3", ImgLoc = "Assets/ChocolateChip.png" });
+
+            CookieList = a;
+        }
+    }
+}
